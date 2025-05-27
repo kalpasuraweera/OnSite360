cc199a6bf355643b906dfde8e75f9c9fb2c81b64
kalpa madhushan <kalpamadhushan18@gmail.com>
2025-05-28 00:29:23 +0530

    frontend setup

---
+++ frontend/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['icon.png'],
+    manifest: {
+      name: 'OnSite360',
+      short_name: 'OnSite360',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#317EFB',
+      icons: [
+        {
+          src: '/icon.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+      ],
+    }
+  })],
+})
