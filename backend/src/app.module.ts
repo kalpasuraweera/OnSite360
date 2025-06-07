423de56aa8165df244aac5c82b58b29f05e29a12
kalpa madhushan <kalpamadhushan18@gmail.com>
2025-06-07 23:11:54 +0530

    refactor authguard

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './projects/projects.module';
 import { UsersModule } from './users/users.module';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './auth/auth.guard';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, ProjectsModule, UsersModule],
+  imports: [AuthModule, UsersModule, ProjectsModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard, // Global AuthGuard to protect all routes
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,10 +4,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
     UsersModule,
+    ConfigModule.forRoot(), // Load environment variables
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
@@ -20,7 +23,13 @@ import { UsersModule } from '../users/users.module';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard, // Global AuthGuard to protect all routes
+    },
+  ],
   exports: [AuthService, JwtModule], // Export JwtModule to make JwtService available
 })
 export class AuthModule {}
