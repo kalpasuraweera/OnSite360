2b8d66fafcdbf3c2abc9df875a0c9e611fd9e315
kalpa madhushan <kalpamadhushan18@gmail.com>
2025-06-07 15:46:24 +0530

    add users module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './projects/projects.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, ProjectsModule],
+  imports: [AuthModule, ProjectsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
