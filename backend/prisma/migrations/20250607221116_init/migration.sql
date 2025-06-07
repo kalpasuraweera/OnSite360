0fcf85cf5727ca01172fefb329f1a82e96ae5927
kalpa madhushan <kalpamadhushan18@gmail.com>
2025-06-08 03:42:27 +0530

    fix db

---
+++ backend/prisma/migrations/20250607221116_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "projectId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "ownerId" TEXT NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("projectId")
+);
