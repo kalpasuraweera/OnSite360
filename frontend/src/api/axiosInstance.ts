382657eb9cfbb5861e822e824df6b116744095e2
kalpa madhushan <kalpamadhushan18@gmail.com>
2025-06-08 14:39:25 +0530

    add env files

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 import { useAuthStore } from "../stores/useAuthStore";
 
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/";
 
 const instance = axios.create({
   baseURL: BASE_URL,
