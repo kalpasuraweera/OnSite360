1e56db1cad455bb9e88933e1456f9c650c07d87b
Piyumi <piyuminavodya2003@gmail.com>
2025-06-08 14:25:27 +0530

    add daisy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['icon.png'],
-    manifest: {
-      name: 'OnSite360',
-      short_name: 'OnSite360',
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#ffffff',
-      theme_color: '#317EFB',
-      icons: [
-        {
-          src: '/icon.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-      ],
-    }
-  })],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icon.png"],
+      manifest: {
+        name: "OnSite360",
+        short_name: "OnSite360",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#317EFB",
+        icons: [
+          {
+            src: "/icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+    tailwindcss(),
+  ],
+});
