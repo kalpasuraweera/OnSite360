b4f3c894ac249bc333857536d8cab42bbce8f743
Piyumi <piyuminavodya2003@gmail.com>
2025-07-19 15:56:29 +0530

    Refactor Login component layout and improve error message handling

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -31,9 +31,9 @@ export default function Login() {
   };
 
   return (
-    <div className="p-5 bg-base-100 flex justify-center h-full items-center w-full">
-      <div className="w-full h-full rounded-4xl bg-gradient-to-r gap-4 from-primary to-white flex p-4">
-        <div className="relative">
+    <div className="lg:p-1 bg-base-100 flex justify-center h-full items-center w-full">
+      <div className="w-full  rounded-4xl gap-1 flex ">
+        <div className="hidden lg:relative lg:flex">
           <div className="flex flex-col gap-5 text-white  absolute z-40 p-20 w-full backdrop-blur-md rounded-3xl">
             <div>
               <h1 className="text-5xl font-bold">Welcome to OnSite360</h1>
@@ -55,28 +55,33 @@ export default function Login() {
             alt="construction-img"
           />
         </div>
-        <div className="bg-base-200 flex flex-col flex-1 rounded-3xl p-5">
+        <div className="bg-base-200 h-screen flex flex-col flex-1 rounded-3xl p-5 lg:p-5">
           <Button variant="back" />
-          <div className="flex justify-center mt-20 lg:mt-28">
+          <div className="flex justify-center mt-36">
             <img src="/logo.png" alt="" className="w-80 flex justify-center" />
           </div>
 
           {error && <ErrorMsg>{error}</ErrorMsg>}
 
-          <form onSubmit={handleLogin} className="space-y-4 px-10">
-            <TextInput
-              id="email"
-              label="Email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <PasswordInput
-              id="password"
-              label="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+          <form
+            onSubmit={handleLogin}
+            className="flex flex-col justify-evenly h-screen p-5 lg:px-10"
+          >
+            <div className="flex gap-5 flex-col">
+              <TextInput
+                id="email"
+                label="Email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <PasswordInput
+                id="password"
+                label="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
             <Button
               type="submit"
               disabled={loading}
@@ -92,59 +97,5 @@ export default function Login() {
         </div>
       </div>
     </div>
-    // <div className="flex lg:items-center bg-base-100 justify-center  p-3 lg:p-10 mx-auto">
-    //   <div className="flex w-full justify-center">
-    //     <div className="lg:w-1/2 flex flex-col min-h-screen gap-8 justify-center bg-base-200 px-3 lg:p-10 rounded-3xl lg:rounded-l-3xl border border-base-300">
-    //       <Button variant="back" />
-    //       <div className="flex justify-center mt-20 lg:mt-28">
-    //         <img src="/logo.png" alt="" className="w-80 flex justify-center" />
-    //       </div>
-
-    //       {/* Heading */}
-    //       <div className="flex flex-col text-center lg:gap-2 justify-center items-center mb-6">
-    //         <h1 className="text-2xl lg:text-3xl font-bold ">
-    //           Let’s accomplish more together
-    //         </h1>
-    //         <p className="text-neutral text-sm">
-    //           Create a user name and a password to activate your account
-    //         </p>
-    //       </div>
-
-    //       {error && <ErrorMsg>{error}</ErrorMsg>}
-
-    //       <form onSubmit={handleLogin} className="space-y-4">
-    //         <TextInput
-    //           id="email"
-    //           label="Email"
-    //           type="email"
-    //           value={email}
-    //           onChange={(e) => setEmail(e.target.value)}
-    //         />
-    //         <PasswordInput
-    //           id="password"
-    //           label="Password"
-    //           value={password}
-    //           onChange={(e) => setPassword(e.target.value)}
-    //         />
-    //         <Button
-    //           type="submit"
-    //           disabled={loading}
-    //           className="btn-primary w-full"
-    //         >
-    //           {loading ? (
-    //             <span className="loading loading-spinner text-primary"></span>
-    //           ) : (
-    //             "Login"
-    //           )}
-    //         </Button>
-    //       </form>
-    //     </div>
-    //     <img
-    //       src="/bg2.jpg"
-    //       className="hidden lg:block w-1/2 rounded-r-3xl"
-    //       alt="construction-img"
-    //     />
-    //   </div>
-    // </div>
   );
 }
